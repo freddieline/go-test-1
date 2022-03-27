cdcf6954a3b3f786c134f1a63afc59e3b9c94690
Freddie Line <freddieline@hotmail.com>
2022-03-27 16:18:50 +0100

    fix: update go.mod

--- go.mod
+++ go.mod
@@ -2,5 +2,3 @@ module warehouse-stocks-checker
 
 go 1.18
 
-require internal/infrastructure/warehouse-api v0.0.0
-replace internal/infrastructure/warehouse-api => ./internal/infrastructure/warehouse-api
--- internal/boot/container.go
+++ internal/boot/container.go
@@ -1,10 +1,9 @@
 package boot
 
 import (
-	warehouse_api "internal/infrastructure/warehouse-api"
+	warehouse_api "github.com/freddieline/go-test-1/internal/infrastructure/warehouse-api"
 	"net/http"
 )
-
 
 func (c *Container) WarehouseAPIClient() warehouse_api.Client {
 	wc := &APIClient{
